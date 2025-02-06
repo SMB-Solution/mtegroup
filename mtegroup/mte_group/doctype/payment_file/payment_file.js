1c48cf24b7a17b809e3d1415ca8be87893ac01e0
Tahir Kapadia <tahir_frappe@LAPTOP-84GHHCHH>
2025-02-06 17:34:38 +0530

    New Doctype Payment File for mtegroup

---
+++ mtegroup/mte_group/doctype/payment_file/payment_file.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Tahir Kapadia and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Payment File", {
+// 	refresh(frm) {
+
+// 	},
+// });
