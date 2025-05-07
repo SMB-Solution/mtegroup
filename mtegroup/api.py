99e711b34d86320fcd919f47cec2b5f367aaaa01
Tahir Kapadia <tahir_frappe@LAPTOP-84GHHCHH>
2025-05-07 15:58:11 +0530

    Updated api.py for JE cancellation.

--- mtegroup/api.py
+++ mtegroup/api.py
@@ -4,10 +4,19 @@ import frappe
 
 @frappe.whitelist()
 def cancel_linked_journal_entry(expense_claim_name):
+    # 1. load the Expense Claim
     doc = frappe.get_doc("MTE Expense Claims", expense_claim_name)
+
+    # 2. if there's a linked JE
     if doc.journal_entry:
+        je_name = doc.journal_entry
+
+        # 3. clear the link field in the Expense Claim
+        frappe.db.set_value(doc.doctype, doc.name, "journal_entry", "")
+
+        # 4. now load & cancel the JE
         try:
-            je = frappe.get_doc("Journal Entry", doc.journal_entry)
+            je = frappe.get_doc("Journal Entry", je_name)
             if je.docstatus == 1:
                 je.cancel()
                 return f"Journal Entry {je.name} cancelled."
